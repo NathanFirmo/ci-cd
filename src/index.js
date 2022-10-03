4e39947d219b818eb39abcea1ba902d0901934f6
NathanFirmo <nathanssfirmo@gmail.com>
2022-10-03 17:40:24 -0300

    feat: change error condition

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // Comment
 module.exports = {
   sum: (a, b) => {
-    if (process.env.SOME_VAR !== 'teste') throw new Error('Missing env var')
+    if (process.env.SOME_VAR !== 'teste' || process.env.SOME_VAR2 !== 'teste') throw new Error('Missing env var')
     return  a + b
   }
 }
