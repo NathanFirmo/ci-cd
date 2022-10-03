b01c608ebb7c6382945863ca8493c4491a27413c
NathanFirmo <nathanssfirmo@gmail.com>
2022-10-03 12:33:52 -0300

    fix: import

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,9 +1,9 @@
 const { sum } = require('./index')
-import { config } from 'dotenv'
+const dotenv = require('dotenv')
 
 describe('Sum of two numbers', () => {
   it('should be able to sum', () => {
-    config({ path: process.cwd() + '.env' })
+    dotenv.config({ path: process.cwd() + '.env' })
     expect(sum(2, 2)).toBe(4)
   })
 })
