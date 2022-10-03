918af7cae5f60926c2dd9cc9999ea4f3a8569b39
NathanFirmo <nathanssfirmo@gmail.com>
2022-10-03 12:29:25 -0300

    test: log process.env

--- src/index.spec.js
+++ src/index.spec.js
@@ -2,6 +2,9 @@ const { sum } = require('./index')
 
 describe('Sum of two numbers', () => {
   it('should be able to sum', () => {
+    console.log({
+      env: process.env
+    })
     expect(sum(2, 2)).toBe(4)
   })
 })
