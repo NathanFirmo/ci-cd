52a718f358e599c6cd67622509f7816363e72340
NathanFirmo <nathanssfirmo@gmail.com>
2022-10-03 12:38:55 -0300

    fix: dotenv path

--- src/index.spec.js
+++ src/index.spec.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv')
 
 describe('Sum of two numbers', () => {
   it('should be able to sum', () => {
-    dotenv.config({ path: process.cwd() + '.env' })
+    dotenv.config({ path: process.cwd() + '/.env' })
     expect(sum(2, 2)).toBe(4)
   })
 })
