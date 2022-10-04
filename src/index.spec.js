05f1a36913a01b3209726c5df6293f6a8365dd39
NathanFirmo <nathanssfirmo@gmail.com>
2022-10-04 11:31:24 -0300

    test: update health check

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,6 +1,6 @@
 const { sum } = require('./index')
 const dotenv = require('dotenv')
-const { exec } = require('child_process')
+const { execSync } = require('child_process')
 
 describe('Sum of two numbers', () => {
   it('should be able to sum', () => {
@@ -8,13 +8,9 @@ describe('Sum of two numbers', () => {
     expect(sum(2, 2)).toBe(4)
   })
 
-  it('should be able to connect with database', async () => {
-    exec(
-      `MYSQL_PWD=mysqlroot mysql --host=127.0.0.1 --port=3346 -u root -e "SELECT 1"`,
-      (err, stdout) => {
-        expect(err).toBeFalsy()
-        expect(stdout).toBeDefined()
-      }
+  it('should be able to connect with database', () => {
+    execSync(
+      `MYSQL_PWD=mysqlroot mysql --host=127.0.0.1 --port=3346 -u root -e "SELECT 1"`
     )
   })
 })
