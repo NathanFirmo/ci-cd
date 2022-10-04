6645c93a50905a2158caae457f6af6f5d8be4995
NathanFirmo <nathanssfirmo@gmail.com>
2022-10-04 11:24:04 -0300

    test: run SELECT 1

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,9 +1,20 @@
 const { sum } = require('./index')
 const dotenv = require('dotenv')
+const { exec } = require('child_process')
 
 describe('Sum of two numbers', () => {
   it('should be able to sum', () => {
     dotenv.config({ path: process.cwd() + '/.env' })
     expect(sum(2, 2)).toBe(4)
+  })
+
+  it('should be able to connect with database', async () => {
+    exec(
+      `MYSQL_PWD=mysqlroot mysql --host=127.0.0.1 --port=3346 -u root -e "SELECT 1"`,
+      (err, stdout) => {
+        expect(err).toBeFalsy()
+        expect(stdout).toBeDefined()
+      }
+    )
   })
 })
