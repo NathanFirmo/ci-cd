55acc8a5278355afa07c8f8aed8e5e38dd0c42b9
NathanFirmo <nathanssfirmo@gmail.com>
2022-12-12 17:53:01 -0300

    ci(GLOBAL): test 31

---
+++ .github/scripts/sleepForOtherJob.js
@@ -0,0 +1,18 @@
+module.exports = async ({ github, context, jobName }) => {
+  const envContext = JSON.parse(process.env.GITHUB_CONTEXT)
+  const getStatusOfJob = async (jobName) => {
+    const {
+      data: { jobs },
+    } = await github.rest.actions.listJobsForWorkflowRunAttempt({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      run_id: envContext.run_id,
+      attempt_number: envContext.run_attempt,
+    })
+    const jobToWait = jobs.find((job) => job.name === jobName)
+    return jobToWait.status
+  }
+
+  let jobStatus = getStatusOfJob(jobName)
+  console.log(jobStatus)
+}
