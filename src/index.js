06cf54a9dd86ff2e817bf9a35ae80e34c4c17cba
Nathan Firmo <nathanssfirmo@gmail.com>
2023-01-24 19:18:12 -0300

    Update index.js [skip ci]

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 module.exports = {
   sum: (a, b) => {
+    
     if (process.env.SOME_VAR !== 'teste' || process.env.SOME_VAR2 !== 'teste') throw new Error('Missing env var')
     return  a + b
   }
