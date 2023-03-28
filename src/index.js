b1033d82e47d4db10fe8fe102edf14838e2daf2c
NathanFirmo <nathanssfirmo@gmail.com>
2023-03-28 17:10:42 -0300

    feat

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 module.exports = {
   sum: (a, b) => {
-    
     if (process.env.SOME_VAR !== 'teste' || process.env.SOME_VAR2 !== 'teste') throw new Error('Missing env var')
     return  a + b
   }
